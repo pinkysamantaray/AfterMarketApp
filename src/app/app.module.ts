1741f478a4209166387e55e8d8dd6c70c9336083
akash.jain <akashjain@deloitte.com>
2018-01-08 17:19:15 +0530

    Equipment details page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EquipmentDetailsComponent } from './equipment-details/equipment-details.component';
+
+const appRoutes: Routes = [
+  { path: 'equipmentDetails', component: EquipmentDetailsComponent },
+  {
+    path: 'home',
+    component: DashboardComponent
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    EquipmentDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]

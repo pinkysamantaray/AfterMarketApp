2efdd0d1c3d141d51880e2b7363bda49e9a7e9ee
pinkysamantaray <pisamantaray@deloitte.com>
2018-01-09 12:36:46 +0530

    Used Google Charts Geo Map

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -24,7 +24,7 @@ export class DashboardComponent implements OnInit {
 
     ngAfterViewInit() {
       //############### Google Chart Code starts ###################
-      /*google.charts.load('current', {
+      google.charts.load('current', {
         'packages':['geochart'],
         // Note: you will need to get a mapsApiKey for your project.
         // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
@@ -57,12 +57,12 @@ export class DashboardComponent implements OnInit {
         };
 
         var chart = new google.visualization.GeoChart(document.getElementById('chartdiv'));
-        chart.draw(data, options);*/
+        chart.draw(data, options);
         //############### Google Chart Code ends ###################
         
         //############### amCharts Code starts #####################
-        var targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
-
+        /*var targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
+        
         var map = AmCharts.makeChart( "chartdiv", {
           "type": "map",
           "theme": "patterns",
@@ -109,7 +109,7 @@ export class DashboardComponent implements OnInit {
           "export": {
             "enabled": false
           }
-        } );
+        } );*/
         //############### amCharts Code ends #####################
         
         //############### HighCharts High map Code starts ########
@@ -294,3 +294,4 @@ export class DashboardComponent implements OnInit {
         //############### HighCharts High map Code ends ##########
     }
  }
+}
